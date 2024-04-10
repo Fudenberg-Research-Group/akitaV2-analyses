8b18abb384c3cc0ee089e76cc2c396eeea9c6cf6
Paulina Smaruj <smaruj@usc.edu>
2024-04-10 15:50:38 -0700

    updated sequence shuffling

--- experiments/background_generation/sequence_shuffling/README.md
+++ experiments/background_generation/sequence_shuffling/README.md
@@ -9,3 +9,9 @@ Dir with data: /scratch2/smaruj/shuffling_exp
 
 2. generating scores for each of the shuffled seqs created using the provided tsv which can have as many parameters as specified. With these score, further analysis is done as shown in the analysis notebook
 ```check generate_scores_for_shuffled_seqs.sh for multi-GPU```
+
+
+
+# Shuffled vs unshuffled
+
+```python generate_shuffled_seqs_df.py -f /project/fudenber_735/genomes/mm10/mm10.fa -seq_bed_file /project/fudenber_735/tensorflow_models/akita/v2/data/mm10/sequences.bed --output_filename ./shuffled_600seqs_k8.tsv --num_seqs 600 --shuffle_parameter 8```
---
+++ experiments/background_generation/sequence_shuffling/generate_scores_for_unshuffled_insertions.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Conda env activation
+eval "$(conda shell.bash hook)"
+conda activate basenji_py3.9_tf2.15
+
+# Parse command line arguments, replace with custom parameters
+genome_fasta="/project/fudenber_735/genomes/mm10/mm10.fa" 
+models_dir="/project/fudenber_735/tensorflow_models/akita/v2/models"
+tsv_file="/home1/smaruj/akitaX1-analyses/experiments/background_generation/sequence_shuffling/shuffled_600seqs_k8.tsv"
+chrom_sizes="/project/fudenber_735/genomes/mm10/mm10.fa.sizes"
+out_dir="/scratch1/smaruj/shuffled_unshuffled/unshuffled"
+models="1 2 3" # this is a string with space seperated model numbers examples "0 1 2" or "4 5 6" 
+batch_size=8 
+max_proc=1
+processes=1
+stats="SCD"
+time="0-01:00:00" 
+# constraint="[xeon-6130|xeon-2640v4]"
+
+# Check if genome_fasta file exists
+if [ ! -f "$genome_fasta" ]; then
+    echo "Genome fasta file does not exist."
+    exit 1
+fi
+
+# Check if models_dir directory exists
+if [ ! -d "$models_dir" ]; then
+    echo "Models directory does not exist."
+    exit 1
+fi
+
+# Check if tsv_file file exists
+if [ ! -f "$tsv_file" ]; then
+    echo "TSV file does not exist."
+    exit 1
+fi
+
+for model in $models
+
+do 
+    # infering params_file and model_file paths
+    this_models_dir="${models_dir}";
+    this_models_dir+="/f";
+    this_models_dir+="${model}";
+    this_models_dir+="c0/train/";
+    
+    params_file="${this_models_dir}";
+    params_file+="params.json";
+    
+    model_file="${this_models_dir}";
+    model_file+="model1_best.h5";
+
+    # changing outdir for this model
+    this_out_dir="${out_dir}";
+    this_out_dir+="_m";
+    this_out_dir+="${model}";
+
+    # changing name of the jobs to be submitted
+    name="S_m";
+    name+="${model}";
+    
+    # running multiGPU script
+    python multiGPU-generate_scores_for_unshuffled_insertions.py "${params_file}" "${model_file}" "${tsv_file}" -f "${genome_fasta}" -o "${this_out_dir}" --stats "${stats}" --batch-size "${batch_size}" -p "${processes}" --max_proc "${max_proc}" --name "${name}" --time "${time}"
+    sleep 15
+done
+
+echo All jobs submitted
+
